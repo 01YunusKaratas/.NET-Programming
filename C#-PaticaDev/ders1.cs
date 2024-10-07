5ea8993d593ae7f28be59309e966f446081e80d7
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-10-07 15:38:49 +0300

    Add files via upload

---
+++ C#-PaticaDev/ders1.cs
@@ -0,0 +1,10 @@
+
+// C# 2000 yıllarında ortaya çıkmıştır.Microsoft tarafından geliştirilmiştir.
+//günlük kullanıma ve makine diline aynı yakınlıktadır.
+//JAVA ve C nin güçlü yönlerini ele almıştır C#
+//Nesneye dayalı bir dildir
+// C# interpreter bir dildir.
+//C# ortaseviyeli bir dildir.
+
+
+
