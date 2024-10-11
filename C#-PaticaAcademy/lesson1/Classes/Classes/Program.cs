75ba4bfaee76bbf3d9b43ff505e024d12b118c77
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-10-11 18:36:36 +0300

    Classes
    ı started  Classes for C#

---
+++ C#-PaticaAcademy/lesson1/Classes/Classes/Program.cs
@@ -0,0 +1,62 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            worker w1 = new worker(); //ınstance
+            
+            w1.Show();
+
+        }
+    }
+
+
+
+    class worker
+    {
+
+        public string name { get; set; }
+        public int id { get; set; }
+        public string surname { get; set; }
+        public string department {  get; set; }
+        
+
+        //Static functionlar sınıfın örneğine bağlı kalmadan çalışırlar .Instance oluşturmadan stati methodlara ulaşılır direk.
+
+        public string info()
+        {
+            Console.WriteLine("Enter the name :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the surname :");
+            surname=Console.ReadLine();
+            Console.WriteLine("Enter the department :");
+            department = Console.ReadLine();
+            Console.WriteLine("Enter the ID:");
+            id=int.Parse(Console.ReadLine());
+
+            
+            return $"NAME :{name} \n SURNAME:{surname} \n DEPARTMENT :{department} \n ID :{id}";
+
+        }
+
+        public  void  Show()
+        {
+
+            Console.WriteLine(info());
+
+
+            
+        }
+
+
+
+    }
+}
