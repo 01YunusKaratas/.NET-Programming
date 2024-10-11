a9e1056c90c9ff739c4eaea7785f4597fd39618e
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-10-11 22:35:43 +0300

    EncapsulationAndProperty
    I'm learned about encapsulatıon

---
+++ C#-PaticaAcademy/lesson1/encapsulationAndproperrty/encapsulationAndproperrty/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulationAndproperrty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Encapsulation and Property 
+
+            Console.WriteLine("Enter the  knowledge :");
+            student s1 = new student()
+            {
+               
+                Name = Console.ReadLine(),
+                Surname = Console.ReadLine(),
+                Age = int.Parse(Console.ReadLine()),
+                Id= int.Parse(Console.ReadLine()),
+                Section = int.Parse(Console.ReadLine()),
+            };  //Create Instance for Student
+
+
+
+            s1.show();
+
+            
+
+
+        }
+
+    }
+
+
+    class student
+    {
+        //Variable
+        private string name;
+        private string surname;
+        private int age;
+        private int id;
+        private int section;
+
+
+        //Setter and getter gibi
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int Age { get => age; set => age = value; }
+        public int Id { get => id; set => id = value; }
+        public int Section { get => section; set => section = value; }
+
+
+        //Constructor
+
+        public student(string name , string surname, int age, int id , int section)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.id = id;
+            this.section = section;
+        }
+
+
+        //Empty Constructor
+        public student() { }
+
+
+
+        public void show()
+        {
+            Console.WriteLine($" NAME :{name} \n SURNAME :{surname} \n AGE: {age} \n ID {id} \n SECTION :{section}");
+        } 
+    }    
+
+
+    
+
+}
---
+++ C#-PaticaAcademy/lesson1/encapsulationAndproperrty/encapsulationAndproperrty/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("encapsulationAndproperrty")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("encapsulationAndproperrty")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("ae7512b4-27c0-4530-9020-d81b1274e2ac")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
