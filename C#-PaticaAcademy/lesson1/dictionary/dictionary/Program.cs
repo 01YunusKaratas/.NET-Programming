d7e1da49ab08e1b43bfd5332dfe39b92a0314b54
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-10-11 15:38:20 +0300

    Dictionary

---
+++ C#-PaticaAcademy/lesson1/dictionary/dictionary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary Dersi 
+
+            //Key value pair den oluşur .
+
+            //Key :Value
+
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+
+
+            dic.Add(1, "yunus emre");
+            dic.Add(2, "ömer yavuz");
+            dic.Add(3, "kadir yıldız");
+            dic.Add(4, "emre hasan");
+
+
+            //Dictionary elemanlarına erişim 
+            Console.WriteLine("------------Elemanlara Erişim------------");
+
+            foreach (var i in dic)
+            {
+                Console.WriteLine(i);
+            }
+
+            //count
+
+            Console.WriteLine(dic.Count);
+
+
+            //Contains
+            Console.WriteLine(dic.ContainsKey(1));
+            Console.WriteLine(dic.ContainsValue("emre"));
+
+
+            //KEYS AND VALUES
+
+            foreach (var i in dic.Keys)
+            {
+                Console.WriteLine($"{i}");
+
+            }
+
+
+            foreach (var i in dic.Values)
+            {
+                Console.WriteLine($"{i}");
+
+            }
+
+
+
+        }
+
+    }
+}
