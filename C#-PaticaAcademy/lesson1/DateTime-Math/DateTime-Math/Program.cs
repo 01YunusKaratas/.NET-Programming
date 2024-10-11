9348d557687ab5fa77296e77a344f972336ab134
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-10-11 13:52:45 +0300

    DateTime and Math Library
    I'm learning DateTime and Library

---
+++ C#-PaticaAcademy/lesson1/DateTime-Math/DateTime-Math/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            //DATE TIME 
+            //DateTime yunus = new DateTime();//böyle yaparasak başlangıçı 01.01.0001 alır onun yerine
+
+            DateTime yunus = DateTime.Now;  
+
+
+            Console.WriteLine(yunus.Hour);//şuan ki saati verir
+            Console.WriteLine(yunus.Minute);//şuanki dakikayı verir
+            Console.WriteLine(yunus.Second);//şuanki saniyeyi verir
+            Console.WriteLine(yunus.Millisecond);//milisaniyeyi verir
+
+            Console.WriteLine(yunus.Year);//yılı verir
+            Console.WriteLine(yunus.Month);//yılın ayını verir
+            Console.WriteLine(yunus.Day);//hangi günde oldğumuzu verir
+            Console.WriteLine(yunus.Date); //Günü ayı yılı verir
+
+            Console.WriteLine(yunus);  //sadece bunu yazarsam hem tarihi hem de saati dakika ve saniyeyle beraber verir
+
+            Console.WriteLine(yunus.DayOfYear); //yılın kaçıncı günü olduğunu söyler
+            Console.WriteLine(yunus.ToShortDateString()); //tam tarihi veriyor normal sistemdeki gibi
+
+            Console.WriteLine(yunus.ToShortTimeString()); //direk laptoptaki gibi saati veriyor
+
+
+            //add methodlarıda şuanki zaman üstüne eklemeler yapyıoruz
+
+            //DateTime format
+            Console.WriteLine("DATETİME FORMAT".PadRight(20,'*'));
+            Console.WriteLine(yunus.ToString("dd"));
+            Console.WriteLine(yunus.ToString("G"));
+            Console.WriteLine(yunus.ToString("F"));
+
+
+            //MATH LIBRARY
+
+            Console.WriteLine(Math.Abs(-25));  //eksi değeri direk artı değer yapar
+            Console.WriteLine(Math.Sin(90)); //SİN karşılığını veriyor
+            Console.WriteLine(Math.Round(12.6)); // Hangisine yakınsa ona yuvarlar
+            Console.WriteLine(Math.Ceiling(12.3));//daime yukarı yuvarlar  ceiling 
+            Console.WriteLine(Math.Floor(12.9));//daime aşşağı yuvarlar floor
+
+
+            Console.WriteLine(Math.Min(12,44));//2 sayıdan küçük olanıdönderir
+            Console.WriteLine(Math.Max(33,412));//2 sayıdan büyük olanı dönderir.
+
+            Console.WriteLine(Math.Pow(2, 5));//üssünü alır 
+            Console.WriteLine(Math.Sqrt(81)); //karekökünü alır
+
+            Console.WriteLine(Math.Log(9));// E tabanındaki logartirmik karşılığını getirir.
+
+            Console.WriteLine(Math.Exp(3));//e üzeri verir
+
+
+
+        }
+    }
+}
---
+++ C#-PaticaAcademy/lesson1/DateTime-Math/DateTime-Math/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("DateTime-Math")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DateTime-Math")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a8fed5d1-9523-4c51-9c71-9d5aad5edd9e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
